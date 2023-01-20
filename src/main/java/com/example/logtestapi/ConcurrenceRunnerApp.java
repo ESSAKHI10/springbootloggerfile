f96a323c0cda19f7799789ac7b4368c83a075aad
ESSAKHI10 <79039587+ESSAKHI10@users.noreply.github.com>
2023-01-20 18:01:24 +0100

    comment the stress test

--- src/main/java/com/example/logtestapi/ConcurrenceRunnerApp.java
+++ src/main/java/com/example/logtestapi/ConcurrenceRunnerApp.java
@@ -1,10 +1,10 @@
 package com.example.logtestapi;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
+// import java.time.Duration;
+// import java.time.Instant;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,14 +14,15 @@ import com.example.logtestapi.Service.ITestService;
 
 @Component
 public class ConcurrenceRunnerApp implements CommandLineRunner {
-
-    @Autowired
-    private ITestService asyncService;
+    //
+    // @Autowired
+    // private ITestService asyncService;
 
     @Override
     public void run(String... args) throws Exception {
 
-        asyncService.StressTest(10, "http://localhost:8080/api/v1/user/sayHello");
+        // asyncService.StressTest(10, "http://localhost:8080/api/v1/user/sayHello");
+        // asyncService.StressTest(10, "http://localhost:8080/api/v1/user/all");
     }
 
 }
