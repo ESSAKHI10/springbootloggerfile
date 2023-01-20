9d998ca48e0ee0fb2f017acfba9f4cf7d1608af0
ESSAKHI10 <79039587+ESSAKHI10@users.noreply.github.com>
2023-01-20 17:58:24 +0100

    dpcumentate user entity

--- src/main/java/com/example/logtestapi/Entity/User.java
+++ src/main/java/com/example/logtestapi/Entity/User.java
@@ -11,6 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /*
  * this entity represent the user class
  */
@@ -22,6 +24,7 @@ import javax.persistence.Id;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(example = "1", required = true, description = " id ")
     private Long id;
     private String firstName;
     private String lastName;
